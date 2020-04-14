e79e5c44f9c04df96c81b48a0323b693d6dbd4be
nanoscopic <github@codechild.com>
2020-04-14 14:37:27 -0700

    Update to work on cygwin
    Updated the script to work on cygwin. Automated extraction of the various needed files into TFTP so that does not have to be done manually ( on cygwin at least ).

--- go.pl
+++ go.pl
@@ -1,18 +1,38 @@
 #!/usr/bin/perl -w
 use strict;
 use Data::Dumper;
+use File::Copy qw/cp/;
+
+my $os = $^O;
+my $vbm = "vboxmanage";
+my $tftp_folder = $ENV{"HOME"}  . "/Library/Virtualbox/TFTP";
+my $docyg = 0;
+my $userprof;
+if( $os eq 'cygwin' ) {
+    my $pfiles = $ENV{'PROGRAMFILES'};
+    $pfiles =~ s|\\|/|g;
+    $vbm = qq'"$pfiles/Oracle/VirtualBox/VBoxManage"';
+    $userprof = $ENV{'USERPROFILE'};
+    $userprof =~ s|\\|/|g;
+    $tftp_folder = "$userprof/.VirtualBox/TFTP";
+    if( ! -e $tftp_folder ) {
+        mkdir $tftp_folder;
+    }
+    $docyg = 1;
+}
 
 # brew cask install virtualbox
 
 my $vmName = new_vm_with_unique( 'ios' );
+#my $vmName = "ios-IudX1";
 my $mac = vm_get_mac( $vmName );
 print "Mac: $mac\n";
-run_server();
+#run_server();
 gen_pxe( $vmName, $mac );
 get_pxelinux();
 get_ubuntu();
 
-wait_all();
+#wait_all();
 
 sub get_pxelinux {
     if( ! -e "syslinux.tgz" ) {
@@ -21,9 +41,11 @@ sub get_pxelinux {
         `curl -L -o syslinux.tgz https://www.kernel.org/pub/linux/utils/boot/syslinux/6.xx/syslinux-6.03.tar.gz`;
         print "Done\n";
     }
-    my $tftp_folder = $ENV{"HOME"}  . "/Library/Virtualbox/TFTP";
+    if( ! -e $tftp_folder ) {
+        die "$tftp_folder does not exist";
+    }
     my $gpxe_file = "$tftp_folder/gpxelinux.0";
-    if( ! $gpxe_file ) {
+    if( ! -e $gpxe_file ) {
         my @files = split( /\s+/, "syslinux-6.03/utils/pxelinux-options 2
           syslinux-6.03/bios/core/lpxelinux.0 3
           syslinux-6.03/bios/gpxe/gpxelinux.0 3
@@ -38,29 +60,47 @@ sub get_pxelinux {
             my $file = shift @files;
             my $pathcount = shift @files;
             my $cmd = "tar -xf syslinux.tgz -C $tftp_folder/ --strip=$pathcount $file 2>&1";
+            print "$cmd\n";
             print `tar -xf syslinux.tgz -C $tftp_folder/ --strip=$pathcount $file 2>&1`;
         }
-        
-        # manually copy $tftp_folder/ldlinux.c32 to web/
-        
-        # Run pxelinux-options, --after path-prefix http://[ip address]:8001/ lpxelinux.0
+    }
+    
+    if( -e "$tftp_folder/ldlinux.c32" && ! -e "web/ldlinux.c32" ) {
+        cp( "$tftp_folder/ldlinux.c32", "web/ldlinux.c32" );
+        `"$userprof/.VirtualBox/TFTP/pxelinux-options" --after path-prefix http://10.0.2.2:8001/ "$userprof/.VirtualBox/TFTP/lpxelinux.0"`;
     }
 }
 
 sub get_ubuntu {
     if( ! -e "ubuntu18.iso" ) {
         #`curl -L -o ubuntu18.iso http://releases.ubuntu.com/18.04.2/ubuntu-18.04.2-server-amd64.iso`;
-        `curl -L -o ubuntu18.iso http://cdimage.ubuntu.com/ubuntu/releases/18.04/release/ubuntu-18.04.2-server-amd64.iso`;
-        # Do the following manually also:
-        # hdiutil attach -nomount ubuntu18.iso
-        # mkdir mnt
-        # mount_cd9660 /dev/disk2 ./mnt
+        `curl -L -o ubuntu18.iso http://cdimage.ubuntu.com/ubuntu/releases/18.04/release/ubuntu-18.04.4-server-amd64.iso`;
         
-        # cp mnt/install/netboot/ubuntu-installer/amd64/initrd.gz web/boot/ubuntu18/
-        # cp mnt/install/netboot/ubuntu-installer/amd64/linux web/boot/ubuntu18/
-        # cp web/boot/ubuntu18/linux web/boot/ubuntu18/vmlinuz
-        # cp mnt/install/filesysten.squashfs web/boot/ubuntu18/
+        my $ddir = "web/boot/ubuntu18";
+        mkdir $ddir if( ! -d $ddir );
+        if( $os eq 'cygwin' ) {
+            isoinfoX("ubuntu18.iso","/INSTALL/NETBOOT/UBUNTU_INSTALLER/AMD64/INITRD.GZ","$ddir/initrd.gz");
+            isoinfoX("ubuntu18.iso","/INSTALL/NETBOOT/UBUNTU_INSTALLER/AMD64/LINUX","$ddir/linux");
+            cp( "$ddir/linux", "$ddir/vmlinuz" );
+            isoinfoX("ubuntu18.iso","/INSTALL/FILESYSTEM.SQUASHFS","$ddir/filesystem.squashfs");
+        }
+        else {
+            # Do the following manually also:
+            # hdiutil attach -nomount ubuntu18.iso
+            # mkdir mnt
+            # mount_cd9660 /dev/disk2 ./mnt
+            
+            # cp mnt/install/netboot/ubuntu-installer/amd64/initrd.gz web/boot/ubuntu18/
+            # cp mnt/install/netboot/ubuntu-installer/amd64/linux web/boot/ubuntu18/
+            # cp web/boot/ubuntu18/linux web/boot/ubuntu18/vmlinuz
+            # cp mnt/install/filesysten.squashfs web/boot/ubuntu18/
+        }
     }
+}
+
+sub isoinfoX {
+    my ( $src, $path, $dest ) = @_;
+    system("isoinfo -i \"$src\" -x \"$path;1\" > \"$dest\"");
 }
 
 sub run_server {
@@ -89,11 +129,18 @@ sub new_vm_with_unique {
     }
     die "Could not find unique name for new VM" if( !$vmName );
     
-    print `vboxmanage createvm \\
+    print `$vbm createvm \\
       --name "$vmName" \\
       --ostype "Linux_64" \\
       --register`;
       
+    #if( $docyg ) {
+    #    print `$vbm modifyvm \\
+    #      "$vmName" \\
+    #      --nattftpfile1 \\
+    #      lpxelinux.0`;
+    #}
+    
     modvm( $vmName, {
         memory => 1500,
         vram => 32,
@@ -105,17 +152,17 @@ sub new_vm_with_unique {
         nattftpfile1 => "lpxelinux.0"
     } );
       
-    print `vboxmanage createhd \\
+    print `$vbm createhd \\
       --filename "$vmName.vdi" \\
       --size 16384`;
       
-    print `vboxmanage storagectl "$vmName" \\
+    print `$vbm storagectl "$vmName" \\
       --name "SATA Controller" \\
       --add sata \\
       --controller IntelAHCI \\
       --portcount 1`;
       
-    print `vboxmanage storageattach "$vmName" \\
+    print `$vbm storageattach "$vmName" \\
       --storagectl "SATA Controller" \\
       --port 0 \\
       --device 0 \\
@@ -132,13 +179,13 @@ sub modvm {
         my $val = $ops->{ $key };
         $opstr .= " --$key $val";
     }
-    print `vboxmanage modifyvm "$vm" $opstr`;
+    print `$vbm modifyvm "$vm" $opstr`;
 }
 
 sub gen_pxe {
     my ( $vmName, $mac ) = @_;
-    my $tftp_folder = $ENV{"HOME"}  . "/Library/Virtualbox/TFTP";
-    if( ! -e $tftp_folder ) {
+    #my $tftp_folder = $ENV{"HOME"}  . "/Library/Virtualbox/TFTP";
+    if( !$docyg && ! -e $tftp_folder ) {
         `sudo mkdir $tftp_folder`;
         my $user = $<;
         my @gps = split( ' ', $) );
@@ -168,7 +215,7 @@ sub gen_pxe {
     
     my $append = "root=$root initrd=$initrd ks=$ksUrl preseed/url=$psUrl ip=dhcp ksdevice=eth0 net.ifnames=0 ipv6.disable=1 VMNAME=$vmName --- quiet";
     
-    open( my $fh, ">$pxe_file" );
+    open( my $fh, ">$pxe_file" ) or die "Cannot write to $pxe_file";
     print $fh "DEFAULT scripted
 label scripted
 menu label Scripted Installation
@@ -180,6 +227,7 @@ APPEND $append";
 sub vm_exists {
     my $vmName = shift;
     my $vms = vb_list_vms();
+    print Dumper( $vms );
     for my $vm ( @$vms ) {
         return $vm->{uuid} if( $vm->{name} eq $vmName );
     }
@@ -198,10 +246,10 @@ sub vm_get_mac {
 }
 
 sub vb_list_vms {
-    my @lines = `vboxmanage list vms`;
+    my @lines = `$vbm list vms`;
     my @vms;
     for my $line ( @lines ) {
-        $line =~ m/^"(.+)" {(.+)}\n$/;
+        $line =~ m/^"(.+)" \{(.+)\}/;
         push( @vms, { name => $1, uuid => $2 } );
     }
     return \@vms;
@@ -209,7 +257,7 @@ sub vb_list_vms {
 
 sub vb_showvminfo {
     my $vmName = shift;
-    my @lines = `vboxmanage showvminfo "$vmName" --machinereadable`;
+    my @lines = `$vbm showvminfo "$vmName" --machinereadable`;
     return \@lines;
 }
 
